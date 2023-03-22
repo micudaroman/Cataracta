c6518cedbd18975f554830033cc6b80eb44b720c
alienCarpet14 <danko.tomas.ts@gmail.com>
2023-03-22 16:02:29 +0100

    videoPlay - playing sound, not playing video

--- app/src/main/java/com/example/cataracta/MediaPlayerActivity.kt
+++ app/src/main/java/com/example/cataracta/MediaPlayerActivity.kt
@@ -2,73 +2,67 @@ package com.example.cataracta
 
 
 //import android.R
+
 import android.content.Intent
-import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import java.io.File
-import android.widget.VideoView;
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.ui.PlayerView
+
 
 //todo add videoview
 class MediaPlayerActivity : AppCompatActivity() {
-    lateinit var videoView: VideoView
-    private var mediaPlayer: MediaPlayer? = null
     private lateinit var videoUri: Uri
     private var intent: Intent? = null
-//    getIntent()
     private var path: String? = null
-    private var videoFile: File? = null
 
 
-//    private var mediaPlayer: MediaPlayer? = null
-//    private var videoView: VideoView? = null
-    private var playButton: Button? = null
-    private val pauseButton: Button? = null
-
-//    var mediacontroller: MediaController? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    Log.d(TAG, "mediaPlayer.onCreate()")
-    setContentView(R.layout.activity_media_player)
-
-    intent = getIntent()
-    path = intent?.getStringExtra("path")
-    Log.d(TAG, "mediaPlayer.onCreate()++")
-    Log.d(TAG, "$path")
-    videoFile = File(path)
-
-    mediaPlayer = MediaPlayer.create(this, Uri.parse(videoFile?.absolutePath));
-    videoView = findViewById(R.id.video_view);
-
-
-    videoView.setVideoPath(videoFile!!.absolutePath)
-//        videoView.setVideoURI(Uri.parse(videoFile!!.absolutePath));
-    mediaPlayer?.start()
-
-//    var playButton = findViewById<Button>(R.id.play_button)
-//    playButton.setOnClickListener {
-//        Log.d(TAG, "playButton++")
-//        videoView.visibility = View.VISIBLE
-//    }
+        Log.d(TAG, "mediaPlayer.onCreate()")
+        // Set up the player instance
+        setContentView(R.layout.activity_media_player)
+        val player: ExoPlayer = ExoPlayer.Builder(this).build()
+        try{
+        val playerView : PlayerView = findViewById(R.id.playerView)
+            if(playerView == null)
+            Log.d(TAG, "null")
+        }
+        catch (e: Exception){
+            Log.d(TAG, e.toString())
+        }
+//        val surface =  SurfaceView(this)
 //
-//
-//    var pauseButton = findViewById<Button>(R.id.pause_button);
-//    pauseButton.setOnClickListener {
-//        Log.d(TAG, "pauseButton++")
-//        mediaPlayer?.pause();
-//        videoView.setVisibility(View.INVISIBLE);
-//    }
+//        player.setVideoSurfaceView(surface)
+//        playerView.player = player
+        intent = getIntent()
+        path = intent?.getStringExtra("path")
+        Log.d(TAG, "mediaPlayer.onCreate()++")
+        Log.d(TAG, "$path")
+        videoUri = Uri.parse(path)
+        // Build the media item.
+        // val mediaItem: MediaItem = MediaItem.fromUri(uri)
+        val mediaItem = MediaItem.Builder()
+            .setUri(videoUri)
+            .build()
+        // Set the media item to be played.
+        player.setMediaItem(mediaItem)
+        // Prepare the player.
+        player.prepare()
+        // Start the playback.
+        player.playWhenReady = true
+
+
+
+
 
 }
 
     override fun onDestroy() {
         super.onDestroy()
-        mediaPlayer?.release()
-        mediaPlayer = null
     }
 
     override fun onStart() {
@@ -80,7 +74,6 @@ class MediaPlayerActivity : AppCompatActivity() {
 
     override fun onPause() {
         super.onPause()
-        mediaPlayer?.pause()
     }
 
     override fun onResume(){
