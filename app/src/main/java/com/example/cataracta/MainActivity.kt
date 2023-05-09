449c2b20fea3a5f651e546dc1a4e00324e542fc8
Roman Mičuda <micuda100@gmail.com>
2023-05-09 12:12:02 +0200

    It doesnt work

--- app/src/main/java/com/example/cataracta/MainActivity.kt
+++ app/src/main/java/com/example/cataracta/MainActivity.kt
@@ -1,260 +1,61 @@
 package com.example.cataracta
 
-
-import android.Manifest
-import android.content.ContentValues
 import android.content.pm.PackageManager
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
-import android.media.AudioManager
-import android.media.ToneGenerator
-import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.WindowManager
-import android.widget.PopupWindow
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.camera.core.Camera
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.video.*
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.content.PermissionChecker
-import androidx.media3.common.util.UnstableApi
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.cataracta.databinding.ActivityMainBinding
-import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
+import android.Manifest
+import android.os.Build
+import androidx.appcompat.widget.Toolbar
 
 
-typealias LumaListener = (luma: Double) -> Unit
+class MainActivity : AppCompatActivity(){
 
-
-@UnstableApi class MainActivity : AppCompatActivity(), SensorEventListener {
-    var flashLightStatus: Boolean = false
     private lateinit var viewBinding: ActivityMainBinding
-
-    private var imageCapture: ImageCapture? = null
-
-    private var videoCapture: VideoCapture<Recorder>? = null
-    private var recording: Recording? = null
-    private var camera: Camera? = null
-
     private lateinit var cameraExecutor: ExecutorService
-
-    private lateinit var sensorManager: SensorManager
-    private var brightness: Sensor? = null
-    private lateinit var lumens: TextView
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(viewBinding.root)
-
-        //disable night mode
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-
-        // Request camera permissions
         if (allPermissionsGranted()) {
-            startCamera()
-        } else {
+            setContentView(R.layout.activity_main)
+        }else{
             ActivityCompat.requestPermissions(
                 this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
 
-        // Set up the listeners for take photo and video capture buttons
-        viewBinding.videoCaptureButton.setOnClickListener { captureVideo() }
 
-        cameraExecutor = Executors.newSingleThreadExecutor()
+        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
 
-        viewBinding.infoButton.setOnClickListener {
-            showPopup()
-        }
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.recordFragment) as NavHostFragment? ?: return
+        val navController = host.navController
 
-        lumens = findViewById(R.id.lumen)
-
-        setupSensorStuff()
     }
 
-    /// LIGHT SENSOR [start]
-    private fun setupSensorStuff() {
-        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
-        brightness = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
-    }
-
-    override fun onSensorChanged(event: SensorEvent?) {
-        if (event?.sensor?.type == Sensor.TYPE_LIGHT) {
-            val light = event.values[0].toInt()
-            lumens.text = light.toString()
-        }
-    }
-
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        return
-    }
-
-    override fun onResume() {
-        super.onResume()
-        sensorManager.registerListener(this, brightness, SensorManager.SENSOR_DELAY_NORMAL)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        sensorManager.unregisterListener(this)
-    }
-    /// LIGHT SENSOR [end]
-
-    // Implements VideoCapture use case, including start and stop capturing.
-    private fun captureVideo() {
-        val videoCapture = this.videoCapture ?: return
-
-        viewBinding.videoCaptureButton.isEnabled = false
-
-        val curRecording = recording
-
-        camera?.cameraControl?.enableTorch(true)
-
-        if (curRecording != null) {
-            // Stop the current recording session.
-            curRecording.stop()
-            camera?.cameraControl?.enableTorch(false)
-            recording = null
-            return
-        }
-
-        // Noise
-        val toneGen = ToneGenerator(AudioManager.STREAM_MUSIC, 1000)
-        toneGen.startTone(ToneGenerator.TONE_CDMA_MED_L, 500)
-
-        // create and start a new recording session
-        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
-            .format(System.currentTimeMillis())
-        val contentValues = ContentValues().apply {
-            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
-            put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4")
-            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-                put(MediaStore.Video.Media.RELATIVE_PATH, "Movies/CameraX-Video")
-            }
-        }
-
-        val mediaStoreOutputOptions = MediaStoreOutputOptions
-            .Builder(contentResolver, MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
-            .setContentValues(contentValues)
-            .build()
-        recording = videoCapture.output
-            .prepareRecording(this, mediaStoreOutputOptions)
-            .apply {
-                if (PermissionChecker.checkSelfPermission(this@MainActivity,
-                        Manifest.permission.RECORD_AUDIO) ==
-                    PermissionChecker.PERMISSION_GRANTED)
-                {
-                    withAudioEnabled()
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 }
-            }
-            .start(ContextCompat.getMainExecutor(this)) { recordEvent ->
-                when(recordEvent) {
-                    is VideoRecordEvent.Start -> {
-                        viewBinding.videoCaptureButton.apply {
-                            text = getString(R.string.stop_capture)
-                            isEnabled = true
-                        }
-                    }
-                    is VideoRecordEvent.Finalize -> {
-                        if (!recordEvent.hasError()) {
-                            val msg = "Video capture succeeded: " +
-                                    "${recordEvent.outputResults.outputUri}"
-                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT)
-                                .show()
-                            Log.d(TAG, msg)
-
-//                            intent = Intent(this, MediaPlayerActivity::class.java)
-//                            intent.putExtra("path", "/storage/emulated/0/Movies/CameraX-Video/$name.mp4")
-//                            startActivity(intent)\
-
-
-                            Log.d(TAG+"this file will be played", "/storage/emulated/0/Movies/CameraX-Video/$name.mp4")
-
-                            val bundle = Bundle()
-                            bundle.putString("path", "/storage/emulated/0/Movies/CameraX-Video/$name.mp4")
-                            val mediaPlayerFragment = MediaPlayerFragment()
-                            mediaPlayerFragment.arguments = bundle
-
-//                            val transaction: FragmentTransaction =
-//                                supportFragmentManager.beginTransaction()
-//                            transaction.replace(R.id.container, mediaPlayerFragment)
-//                            transaction.addToBackStack(null)
-//                            transaction.commit()
-//                            Log.d(TAG, "failure")
-
-
-                        } else {
-                            recording?.close()
-                            recording = null
-                            Log.e(TAG, "Video capture ends with error: " +
-                                    "${recordEvent.error}")
-                        }
-                        viewBinding.videoCaptureButton.apply {
-                            text = getString(R.string.start_capture)
-                            isEnabled = true
-                        }
-                    }
-                }
-            }
-    }
-
-    private fun startCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-
-        cameraProviderFuture.addListener({
-            // Used to bind the lifecycle of cameras to the lifecycle owner
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-
-            // Preview
-            val preview = Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
-                }
-
-            // ImageCapture
-            imageCapture = ImageCapture.Builder()
-                .build()
-
-            // VideoCapture
-            val recorder = Recorder.Builder()
-                .setQualitySelector(QualitySelector.from(Quality.UHD))
-                .build()
-            videoCapture = VideoCapture.withOutput(recorder)
-
-            // Select back camera as a default
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            try {
-                // Unbind use cases before rebinding
-                cameraProvider.unbindAll()
-
-                // Bind use cases to camera
-                camera = cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageCapture, videoCapture)
-
-
-            } catch(exc: Exception) {
-                Log.e(TAG, "Use case binding failed", exc)
-            }
-
-        }, ContextCompat.getMainExecutor(this))
+            }.toTypedArray()
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
@@ -268,7 +69,7 @@ typealias LumaListener = (luma: Double) -> Unit
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
             if (allPermissionsGranted()) {
-                startCamera()
+                setContentView(R.layout.activity_main)
             } else {
                 Toast.makeText(this,
                     "Permissions not granted by the user.",
@@ -277,7 +78,6 @@ typealias LumaListener = (luma: Double) -> Unit
             }
         }
     }
-
     override fun onDestroy() {
         super.onDestroy()
         cameraExecutor.shutdown()
@@ -285,30 +85,7 @@ typealias LumaListener = (luma: Double) -> Unit
 
 
 
-    companion object {
-        private const val TAG = "Cataracta"
-        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
-        private const val REQUEST_CODE_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS =
-            mutableListOf (
-                Manifest.permission.CAMERA,
-                Manifest.permission.RECORD_AUDIO
-            ).apply {
-                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
-                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                }
-            }.toTypedArray()
-    }
 
-    private fun showPopup() {
-        val popupView = LayoutInflater.from(this).inflate(R.layout.info_popup_window, null)
-        val popupWindow = PopupWindow(popupView, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT)
-        popupWindow.isFocusable = true
-        popupWindow.showAsDropDown(viewBinding.popupHelper, 0, 0)
 
-        popupView.findViewById<View>(R.id.info_button_ok).setOnClickListener {
-            popupWindow.dismiss()
-        }
-    }
 }
 
---
+++ app/src/main/java/com/example/cataracta/ui/record/RecordFragment.kt
@@ -0,0 +1,259 @@
+package com.example.cataracta.ui.record
+
+import android.Manifest
+import android.content.ContentResolver
+import android.content.ContentValues
+import android.hardware.SensorEventListener
+import android.media.AudioManager
+import android.media.ToneGenerator
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.camera.core.Camera
+import androidx.camera.video.*
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.cataracta.MainActivity
+import com.example.cataracta.MediaPlayerFragment
+import com.example.cataracta.R
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.Executors
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorManager
+import android.view.WindowManager
+import android.widget.PopupWindow
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import com.example.cataracta.databinding.ActivityMainBinding
+import java.util.concurrent.ExecutorService
+
+
+abstract class RecordFragment: Fragment() {
+    var flashLightStatus: Boolean = false
+    abstract val contentResolver: ContentResolver
+    private var videoCapture: VideoCapture<Recorder>? = null
+    private var recording: Recording? = null
+    private var camera: Camera? = null
+    private lateinit var viewBinding: ActivityMainBinding
+    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var lumens: TextView
+    private var imageCapture: ImageCapture? = null
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.activity_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        viewBinding.videoCaptureButton.setOnClickListener { captureVideo() }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        viewBinding.infoButton.setOnClickListener {
+            showPopup()
+        }
+        Toast.makeText(requireContext(),
+            "Start camera",
+            Toast.LENGTH_SHORT).show()
+
+
+        startCamera()
+
+        lumens = view.findViewById(R.id.lumen)
+
+//        setupSensorStuff()
+    }
+
+//    private fun setupSensorStuff() {
+//        sensorManager = getSystemService(AppCompatActivity.SENSOR_SERVICE) as SensorManager
+//        brightness = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+//    }
+
+    companion object {
+        private const val TAG = "Cataracta"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+    }
+
+    private fun showPopup() {
+//        val popupView = LayoutInflater.from(this).inflate(R.layout.info_popup_window, null)
+//        val popupWindow = PopupWindow(popupView, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT)
+//        popupWindow.isFocusable = true
+//        popupWindow.showAsDropDown(viewBinding.popupHelper, 0, 0)
+//
+//        popupView.findViewById<View>(R.id.info_button_ok).setOnClickListener {
+//            popupWindow.dismiss()
+//        }
+    }
+
+    private fun captureVideo() {
+        val videoCapture = this.videoCapture ?: return
+
+        viewBinding.videoCaptureButton.isEnabled = false
+
+        val curRecording = recording
+
+        camera?.cameraControl?.enableTorch(true)
+
+        if (curRecording != null) {
+            // Stop the current recording session.
+            curRecording.stop()
+            camera?.cameraControl?.enableTorch(false)
+            recording = null
+            return
+        }
+
+        // Noise
+        val toneGen = ToneGenerator(AudioManager.STREAM_MUSIC, 1000)
+        toneGen.startTone(ToneGenerator.TONE_CDMA_MED_L, 500)
+
+        // create and start a new recording session
+        val name = SimpleDateFormat(RecordFragment.FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4")
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Video.Media.RELATIVE_PATH, "Movies/CameraX-Video")
+            }
+        }
+
+        val mediaStoreOutputOptions = MediaStoreOutputOptions
+            .Builder(contentResolver, MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
+            .setContentValues(contentValues)
+            .build()
+        recording = videoCapture.output
+            .prepareRecording(requireContext(), mediaStoreOutputOptions)
+            .apply {
+                if (ContextCompat.checkSelfPermission(requireContext(),
+                        Manifest.permission.RECORD_AUDIO) ==
+                    PackageManager.PERMISSION_GRANTED)
+                {
+                    withAudioEnabled()
+                }
+            }
+            .start(ContextCompat.getMainExecutor(requireContext())) { recordEvent ->
+                when(recordEvent) {
+                    is VideoRecordEvent.Start -> {
+                        viewBinding.videoCaptureButton.apply {
+                            text = getString(R.string.stop_capture)
+                            isEnabled = true
+                        }
+                    }
+                    is VideoRecordEvent.Finalize -> {
+                        if (!recordEvent.hasError()) {
+                            val msg = "Video capture succeeded: " +
+                                    "${recordEvent.outputResults.outputUri}"
+//                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT)
+//                                .show()
+                            Log.d(RecordFragment.TAG, msg)
+
+// tomas
+//                            intent = Intent(this, MediaPlayerActivity::class.java)
+//                            intent.putExtra("path", "/storage/emulated/0/Movies/CameraX-Video/$name.mp4")
+//                            startActivity(intent)\
+
+
+                            Log.d(RecordFragment.TAG +"this file will be played", "/storage/emulated/0/Movies/CameraX-Video/$name.mp4")
+
+                            val bundle = Bundle()
+                            bundle.putString("path", "/storage/emulated/0/Movies/CameraX-Video/$name.mp4")
+//                            val mediaPlayerFragment = MediaPlayerFragment()
+//                            mediaPlayerFragment.arguments = bundle
+
+                            //   tomas
+//                            val transaction: FragmentTransaction =
+//                                supportFragmentManager.beginTransaction()
+//                            transaction.replace(R.id.container, mediaPlayerFragment)
+//                            transaction.addToBackStack(null)
+//                            transaction.commit()
+//                            Log.d(TAG, "failure")
+
+
+                        } else {
+                            recording?.close()
+                            recording = null
+                            Log.e(
+                                RecordFragment.TAG, "Video capture ends with error: " +
+                                    "${recordEvent.error}")
+                        }
+                        viewBinding.videoCaptureButton.apply {
+                            text = getString(R.string.start_capture)
+                            isEnabled = true
+                        }
+                    }
+                }
+            }
+    }
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
+                }
+
+            // ImageCapture
+            imageCapture = ImageCapture.Builder()
+                .build()
+
+            // VideoCapture
+            val recorder = Recorder.Builder()
+                .setQualitySelector(QualitySelector.from(Quality.UHD))
+                .build()
+            videoCapture = VideoCapture.withOutput(recorder)
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                camera = cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture, videoCapture)
+
+
+            } catch(exc: Exception) {
+                Log.e(RecordFragment.TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+}
